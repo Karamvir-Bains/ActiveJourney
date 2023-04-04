dcbfd0cdc6b2de3549c8f6acf5db8dc239f7de0b
Lauren Ashley <la.zaubi@gmail.com>
2023-04-04 12:08:54 -0700

    add form markup

--- src/pages/_login.form.tsx
+++ src/pages/_login.form.tsx
@@ -3,7 +3,7 @@ export default function LoginForm() {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+        <h1 className="text-3xl font-semibold text-center text-blue-700 underline">
             Sign in
         </h1>
         <form className="mt-6">
@@ -15,7 +15,7 @@ export default function LoginForm() {
                 Email
             </label>
             <input type="email"
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
           <div className="mb-2">
@@ -27,14 +27,11 @@ export default function LoginForm() {
             </label>
             <input
               type="password"
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-          <a href="#" className="text-xs text-purple-600 hover:underline">
-            Forget Password?
-          </a>
           <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
               Login
             </button>
           </div>
@@ -45,7 +42,7 @@ export default function LoginForm() {
             Don't have an account?{" "}
             <a
                 href="#"
-                className="font-medium text-purple-600 hover:underline"
+                className="font-medium text-blue-600 hover:underline"
             >
                 Sign up
             </a>
